5816b77761d04a3b57b6c39799ec34352909c756
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2021-12-07 00:16:32 +0200

    task_6_finish

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const input = document.getElementById('validation-input');
+
+const maxLength = input.dataset.length;
+
+input.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event) {
+  if (event.currentTarget.value.length == maxLength) {
+    event.currentTarget.classList.remove('invalid');
+    event.currentTarget.classList.add('valid');
+  } else {
+    event.currentTarget.classList.remove('valid');
+    event.currentTarget.classList.add('invalid');
+  };
+};
