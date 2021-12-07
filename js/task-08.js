bf7b8f90678a96cdee3be0e38842517b1656fef3
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2021-12-07 22:32:17 +0200

    task_8_middle

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+}
 
+refs.form.addEventListener('submit', onFormLogin);
+
+function onFormLogin(event) {
+  event.preventDefault();
+  const {
+    elements: {email, password} 
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Empty field!!!')
+  };
+};
