0e545d743a16bd0cbf533202a9776e7201f9d778
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2021-12-08 18:17:20 +0200

    task_9_finish

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', onButtonChange);
+
+function onButtonChange() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+ // console.log(document.body.style.backgroundColor = getRandomHexColor())
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
