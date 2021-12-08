f89af7c5e0bb407926bad79f05655292bdfa12d9
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2021-12-08 19:47:19 +0200

    task_10_middle

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
+const input = document.getElementById('controls');
+const boxes = document.getElementById('boxes');
+const create = document.querySelector('[data-create]');
+const destroy = document.querySelector('[data-destroy]');
+
+create.addEventListener('click', onInputNumberCreate);
+destroy.addEventListener('click', onInputNumberDestroy);
+
+function onInputNumberCreate(event) {
+  const amount = input.value;
+  createBoxes(amount);
+};
+
+function onInputNumberDestroy(event) {
+  boxes.innerHTML = '';
+};
+
+function createBoxes(amount) {
+  const div = document.createElement('div');
+  
+  for (let i = 0; i < amount; i += 1) {
+    const size = 30 + i * 10;
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+  }
+  boxes.append(div);
+    console.log(boxes.append(div));
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
