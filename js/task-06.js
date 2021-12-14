4c74f25ddf4dd8636c9b1b757e0f28632e34e39c
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2021-12-14 16:39:29 +0200

    task_9_reduct

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,7 @@ const maxLength = input.dataset.length;
 input.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.length == maxLength) {
+  if (event.currentTarget.value.length === maxLength) {
     event.currentTarget.classList.remove('invalid');
     event.currentTarget.classList.add('valid');
   } else {
--- js/task-09.js
+++ js/task-09.js
@@ -5,9 +5,8 @@ button.addEventListener('click', onButtonChange);
 
 function onButtonChange() {
   document.body.style.backgroundColor = getRandomHexColor();
-  color.textContent = getRandomHexColor();
- // console.log(document.body.style.backgroundColor = getRandomHexColor())
-}
+  color.textContent = document.body.style.backgroundColor;
+};
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
